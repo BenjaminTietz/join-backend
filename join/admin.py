bb6cbd24a5c9e08f8b1def9c02a5014cf619c2b3
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-09 14:17:57 +0200

    chore: register Task, SubTask, and Contact models in admin
    - Registered Task, SubTask, and Contact models in the Django admin panel for easier management.

--- join/admin.py
+++ join/admin.py
@@ -1,3 +1,7 @@
+from .models import Task, SubTask, Contact
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Contact)
