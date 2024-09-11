1c375089a744d1eff6e499f08347321ff7eaa284
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-11 16:49:01 +0200

    chore(dependencies): add phone_field package and update requirements.txt
    - Installed 'phone_field' via pip and integrated it into settings.py.
    - Updated requirements.txt using pip freeze to include 'phone_field' and other dependency updates.

--- django_join_backend_app/settings.py
+++ django_join_backend_app/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'join',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'phonenumber_field',
+    'phone_field'
 ]
 
 MIDDLEWARE = [
