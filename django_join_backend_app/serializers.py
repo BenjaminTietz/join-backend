bfa35205e3826f33df67f3172bac459c2eac17fc
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-11 16:47:36 +0200

    feat(api): add ContactSerializer for contact data validation
    - Created ContactSerializer to handle validation and serialization of contact data.
    - Defined fields 'name', 'email', and 'phone' for serialization from the Contact model.

--- django_join_backend_app/serializers.py
+++ django_join_backend_app/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from join.models import Contact
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'id')
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'phone')
