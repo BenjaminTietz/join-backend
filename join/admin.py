7e64822d51509878e3455b75c5371ac373892057
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-12 16:57:37 +0200

    refactor(admin): Add SubTaskInline to TaskAdmin for managing SubTasks within Task
    - Introduced `SubTaskInline` class to the `TaskAdmin` class in `admin.py` to allow managing SubTasks directly within the Task admin interface.
    - `SubTaskInline` is defined as an `admin.TabularInline` to provide a tabular layout for SubTasks associated with each Task.
    - Configured `SubTaskInline` to display one extra empty form for SubTasks by setting `extra = 1`.
    - Updated `TaskAdmin` to include `SubTaskInline`, enabling a seamless experience for managing SubTasks alongside Tasks in the Django admin panel.

--- join/admin.py
+++ join/admin.py
@@ -2,16 +2,25 @@ from .models import Task, SubTask, Contact
 from django.contrib import admin
 
 # Register your models here.
+class SubTaskInline(admin.TabularInline): 
+    model = SubTask
+    extra = 1 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'category', 'priority', 'status', 'due_date', 'created_at')
     fields = ('id', 'title', 'description', 'category', 'priority', 'status', 'due_date', 'created_at')
     readonly_fields = ('id', 'created_at')  
+    inlines = [SubTaskInline]
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'task', 'title', 'checked', 'created_at')
     fields = ('id', 'task', 'title', 'checked', 'created_at')
     readonly_fields = ('id', 'created_at')
+    
+    def task_title(self, obj):
+        return obj.task.title
+    task_title.short_description = 'Task Title'
+    task_title.admin_order_field = 'task__title'
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'phone', 'created_at')
