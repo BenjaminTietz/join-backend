e0b380513dc54a5e9c878eebe7de6243c5e34a3b
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-20 10:29:08 +0200

    feat: Add UserAdmin configuration for custom User model
    - Registered User model with the Django admin using BaseUserAdmin.
    - Configured list display to show email, username, is_staff, and is_active.
    - Implemented fieldsets for user details, permissions, and important dates.
    - Added support for adding new users with necessary fields.
    - Enabled searching by email and username for better usability in the admin panel.

--- join/admin.py
+++ join/admin.py
@@ -1,6 +1,7 @@
-from .models import Task, SubTask, Contact
+from .models import Task, SubTask, Contact, User
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 # Register your models here.
 class SubTaskInline(admin.TabularInline): 
     model = SubTask
@@ -21,3 +22,28 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'phone', 'created_at')
     fields = ('id', 'name', 'email', 'phone', 'created_at')
     readonly_fields = ('id', 'created_at')
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('username',)}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
